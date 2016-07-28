672db61d8376dba0e6102c67a0401d707c9c2656
jjfly <jjfly@willoffice.cn>
2016-07-28 15:13:18 +0800

    修改国家选择排序问题

--- LibView/src/main/java/com/will/view/indexview/PinyinComparator.java
+++ LibView/src/main/java/com/will/view/indexview/PinyinComparator.java
@@ -4,9 +4,9 @@ package com.will.view.indexview;
 import java.util.Comparator;
 
 public class PinyinComparator implements Comparator<Object> {
-	public int compare(Object o1, Object o2) {
-		String str1 = PingYinUtil.getPingYin((String) o1);
-		String str2 = PingYinUtil.getPingYin((String) o2);
-		return str1.compareTo(str2);
-	}
+    public int compare(Object o1, Object o2) {
+        String str1 = PingYinUtil.getPingYin((String) o1);
+        String str2 = PingYinUtil.getPingYin((String) o2);
+        return str1.compareTo(str2);
+    }
 }
