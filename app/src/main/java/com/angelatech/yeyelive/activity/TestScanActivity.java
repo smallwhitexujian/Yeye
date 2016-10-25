99b01bfc5b0fe52422eff403bc306ac6a7766fc7
White <xj626361950@163.com>
2016-10-25 14:57:45 +0800

    修改扫描二维码

--- app/src/main/java/com/angelatech/yeyelive/activity/TestScanActivity.java
+++ app/src/main/java/com/angelatech/yeyelive/activity/TestScanActivity.java
@@ -8,18 +8,11 @@ import android.util.Log;
 import android.view.View;
 
 import com.angelatech.yeyelive.R;
-import com.angelatech.yeyelive.activity.function.MainEnter;
 import com.angelatech.yeyelive.db.model.BasicUserInfoDBModel;
-import com.angelatech.yeyelive.model.CommonModel;
 import com.angelatech.yeyelive.model.WebTransportModel;
 import com.angelatech.yeyelive.util.CacheDataManager;
-import com.angelatech.yeyelive.util.JsonUtil;
 import com.angelatech.yeyelive.util.StartActivityHelper;
 import com.angelatech.yeyelive.view.LoadingDialog;
-import com.angelatech.yeyelive.web.HttpFunction;
-import com.will.web.handle.HttpBusinessCallback;
-
-import java.util.Map;
 
 import cn.bingoogolapple.qrcode.core.QRCodeView;
 import cn.bingoogolapple.qrcode.zbar.ZBarView;
@@ -52,14 +45,12 @@ public class TestScanActivity extends AppCompatActivity implements QRCodeView.De
     private static final String TAG = TestScanActivity.class.getSimpleName();
     private QRCodeView mQRCodeView;
     private BasicUserInfoDBModel userInfo;
-    private MainEnter mainEnter;
     private WebTransportModel webTransportModel;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
         userInfo = CacheDataManager.getInstance().loadUser();
-        mainEnter = new MainEnter(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mQRCodeView = (ZBarView) findViewById(R.id.zbarview);
@@ -108,44 +99,18 @@ public class TestScanActivity extends AppCompatActivity implements QRCodeView.De
         Log.i(TAG, "result:" + result);
         vibrate();
         mQRCodeView.startSpot();
-        String key = result.split("key=")[1];
         String iuresult = null;
         if (result.contains("key=")) {
-            iuresult = result + "?userid=" + userInfo.userid + "?token" + userInfo.token;
+            iuresult = result + "&userid=" + userInfo.userid + "&token" + userInfo.token;
         }
         webTransportModel = new WebTransportModel();
         webTransportModel.url = iuresult;
-        webTransportModel.title = getString(R.string.scan_title);
+        webTransportModel.title = getString(R.string.yeye_web_title);
         if (!webTransportModel.url.isEmpty()) {
+            LoadingDialog.cancelLoadingDialog();
             StartActivityHelper.jumpActivity(TestScanActivity.this, WebActivity.class, webTransportModel);
         }
-//        mainEnter.ScanRecharge(CommonUrlConfig.ScanRecharge, userInfo.userid, userInfo.token, key, callback);
     }
-
-    private HttpBusinessCallback callback = new HttpBusinessCallback() {
-        @Override
-        public void onFailure(Map<String, ?> errorMap) {
-            LoadingDialog.cancelLoadingDialog();
-        }
-
-        @Override
-        public void onSuccess(final String response) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    LoadingDialog.cancelLoadingDialog();
-                    CommonModel results = JsonUtil.fromJson(response, CommonModel.class);
-                    if (results != null) {
-                        if (!HttpFunction.isSuc(results.code)) {
-                            onBusinessFaild(results.code);
-                        } else {
-
-                        }
-                    }
-                }
-            });
-        }
-    };
 
     @Override
     public void onScanQRCodeOpenCameraError() {
