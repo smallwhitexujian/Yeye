aa9ace8da9ee4d1551a5e9efb74f5cb4d068b163
sandy <shanli027@qq.com>
2016-10-27 11:15:02 +0800

    1.弹幕飞屏位置修改，不被礼物连击覆盖 2.用户资料弹出方式修改

---
+++ app/src/main/java/com/angelatech/yeyelive/activity/FansRankActivity.java
@@ -0,0 +1,16 @@
+package com.angelatech.yeyelive.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.angelatech.yeyelive.R;
+
+public class FansRankActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fans_rank);
+    }
+
+}
