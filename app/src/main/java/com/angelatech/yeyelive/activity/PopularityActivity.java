fac8b553dc8d6d37c9326a6545fb914d0bd9c28f
sandy <shanli027@qq.com>
2016-10-27 17:27:17 +0800

    知名度详细页添加

---
+++ app/src/main/java/com/angelatech/yeyelive/activity/PopularityActivity.java
@@ -0,0 +1,16 @@
+package com.angelatech.yeyelive.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.angelatech.yeyelive.R;
+
+public class PopularityActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popularity);
+    }
+
+}
