8d0634ec7464007730c6a4f8e70a188b99df2fc1
xujian <xj626361950@163.com>
2017-01-07 16:06:37 +0800

    删除多余代码

--- app/src/main/java/com/angelatech/yeyelive/activity/MoneyChangerActivity.java
+++ app/src/main/java/com/angelatech/yeyelive/activity/MoneyChangerActivity.java
@@ -162,7 +162,6 @@ public class MoneyChangerActivity extends BaseActivity {
                                                 e.printStackTrace();
                                             }
                                         }
-                                        ToastUtils.showToast(MoneyChangerActivity.this, response);
                                     }
                                 });
                             }
